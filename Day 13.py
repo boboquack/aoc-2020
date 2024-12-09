0f70af0c0700cc96eb2cf884aa57abfe3c9f4f3b
boboquack <boboquack@users.noreply.github.com>
2024-12-09 06:18:06 +0100

    Remove input data

--- Day 13.py
+++ Day 13.py
@@ -1,5 +1,4 @@
-s='''1008141
-17,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,523,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,13,19,x,x,x,23,x,x,x,x,x,x,x,787,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,29'''.split('\n')
+s=INPUT.split('\n')
 s[0]=int(s[0])
 s[1]=s[1].split(',')
 b=999999
